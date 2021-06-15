2f649d4eddea49a8154bf7e9fd58627b5ce9385f
ARSALAN UMAR <64530489+Arxlan40@users.noreply.github.com>
2021-06-15 17:02:58 +0500

    Notes Added Functionality

--- notes.js
+++ notes.js
@@ -1,5 +1,58 @@
-const getNotes = function () {
-    return 'Your notes...'
-}
+const { Console } = require("console");
+const fs = require("fs");
+const chalk = require("chalk");
+const print = console.log;
 
-module.exports = getNotes
+const loadNotes = function () {
+  try {
+    const data = fs.readFileSync("Notes.json");
+    const dataBuffer = data.toString();
+
+    return JSON.parse(dataBuffer);
+  } catch (e) {
+    return [];
+  }
+};
+const getNotes = function () {
+  return "Your notes...";
+};
+const addNotes = function (title, body) {
+  const notes = loadNotes();
+  const duplicate = notes.filter(function (note) {
+    return note.title === title || note.body === body;
+  });
+  if (duplicate.length === 0) {
+    notes.push({
+      title: title,
+      body: body,
+    });
+    print(chalk.green.bold(" Note Added"));
+
+    return savenotes(notes);
+  } else {
+    print(chalk.green.bold("Note Not Added"));
+  }
+};
+const removeNotes = function (title) {
+  const notes = loadNotes();
+  const duplicate = notes.filter(function (note) {
+    return note.title !== title;
+  });
+
+  if (duplicate.length === notes.length) {
+    print(chalk.red.bold("No Note removed"));
+  } else {
+    print(chalk.green.bold("Note removed"));
+
+    return savenotes(duplicate);
+  }
+};
+
+const savenotes = function (notes) {
+  return fs.writeFileSync("Notes.json", JSON.stringify(notes));
+};
+
+module.exports = {
+  addNotes: addNotes,
+  removeNotes: removeNotes,
+};
